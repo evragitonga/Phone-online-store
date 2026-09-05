e7dcbd2075bafe1e236260c3d3c5a43f59673a87
Evra Gitonga <evra.gitonga@student.moringaschool.com>
2026-09-05 21:08:22 +0300

    added the search feature on Productlist component

--- Phone online store/src/components/ProductList.jsx
+++ Phone online store/src/components/ProductList.jsx
@@ -1,12 +1,18 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ProductContext } from "../context/ProductContext"
 import ProductCard from "./ProductCard"
 
 function ProductList(){
- const products = useContext(ProductContext)
+ const [search, setSearch] = useState("")
+ const {data} = useContext(ProductContext)
+ const filteredProducts = data.filter(product => {
+  return product.name.tolowercase().includes(search.tolowercase())
+  
+ })
  return (
   <div>
-    {products.map((product) => {
+    <input type="text" placeholder="Search Product" onChange={(e) => setSearch(e.target.value)} value={search}/>
+    {filteredProducts.map((product) => {
       <ProductCard key={product.id} product={product}/>
     })}
   </div>
