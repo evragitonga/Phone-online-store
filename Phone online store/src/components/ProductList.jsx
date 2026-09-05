34e815c549c92ffc7d411c8d302774cc661e187e
Evra Gitonga <evra.gitonga@student.moringaschool.com>
2026-09-05 21:32:54 +0300

    updated main

--- Phone online store/src/components/ProductList.jsx
+++ Phone online store/src/components/ProductList.jsx
@@ -1,14 +1,20 @@
-import { useContext } from "react"
+import { useContext,useState } from "react"
 import { ProductContext } from "../context/ProductContext"
 import ProductCard from "./ProductCard"
 
 function ProductList(){
+ const [search, setSearch] = useState("")
  const {data} = useContext(ProductContext)
+ const filteredProducts = data.filter(product => {
+  return product.name.toLowerCase().includes(search.toLowerCase())
+  
+ })
  return (
   <div>
-    {data.map((product) => (
+    <input type="text" placeholder="Search Product" onChange={(e) => setSearch(e.target.value)} value={search}/>
+    {filteredProducts.map((product) => {
       <ProductCard key={product.id} product={product}/>
-    ))}
+    })}
   </div>
  )
 }
