05d68dad036d701d3efaa0290c7da9bb7f6d6ccb
Evra Gitonga <evra.gitonga@student.moringaschool.com>
2026-09-06 22:55:00 +0300

    added styling to home and navbar components

--- Phone online store/src/components/Home.jsx
+++ Phone online store/src/components/Home.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router"
+import "../index.css"
 function Home(){
   return(
-    <div>
-      <h1>Welcome to our Phone Online Store</h1>
-      <p>We have all types of phones</p>
-      <Link
-      to="/shop"
-      >Shop</Link>
+    <div className="homePage">
+      <div className="homePageContent">
+        <h1>Welcome to our Phone Online Store</h1>
+        <p>We have all types of phones</p>
+        <Link className="link"
+        to="/shop"
+      > Shop</Link>
+      </div>
+      
     </div>
   )
 }
